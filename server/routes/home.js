d8b31c9a7672f1121645d3d993bd35c20198f340
bill-burgess <wmhburgess@gmail.com>
2017-08-08 16:44:34 +1200

    nested insert

--- server/routes/home.js
+++ server/routes/home.js
@@ -17,9 +17,19 @@ router.post('/', function (req, res) {
     town: req.body.oldTown,
     postcode: req.body.oldPostcode
   }
+  var newAddress = {
+    street: req.body.newStreet,
+    area: req.body.newArea,
+    town: req.body.newTown,
+    postcode: req.body.newPostcode
+  }
   db.postAddress(oldAddress)
-    .then( () => {
-      res.sendStatus(200)
+    .then(function (old_id){
+      db.postAddress(newAddress)
+      .then(function(new_id){
+        console.log("Old ID:" + old_id)
+        console.log("New ID:" + new_id)
+      })
     })
     .catch(err => {
       console.log(err)
