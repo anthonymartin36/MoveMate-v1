82c12db1df1090ef96bed5d8be550fd67a337428
bill-burgess <wmhburgess@gmail.com>
2017-08-18 10:41:17 +1200

    Database tests

--- test/db.test.js
+++ test/db.test.js
@@ -7,7 +7,7 @@ var server = require('../server/server')
 var configureDatabase = require('./helpers/database-config')
 configureDatabase(test, server)
 
-test.cb('connect to the Address table and test the seed', (t) => {
+test.cb('DB Connect: Address table and test Seed', (t) => {
   //console.log("t, dumbass :" + t[0])
   var db = t.context.connection
     db('addresses').then(function(seed){
@@ -16,5 +16,22 @@ test.cb('connect to the Address table and test the seed', (t) => {
     })
 })
 
+test.cb('DB Connect: Job table and test Seed', (t) => {
+  //console.log("t, dumbass :" + t[0])
+  var db = t.context.connection
+    db('jobs').then(function(seed){
+      t.is(seed.length, 1)
+      t.end()
+    })
+})
+
+test.cb('DB Connect: job-details table and test Seed', (t) => {
+  //console.log("t, dumbass :" + t[0])
+  var db = t.context.connection
+    db('job-details').then(function(seed){
+      t.is(seed.length, 1)
+      t.end()
+    })
+})
 
 //https://github.com/avajs/ava
